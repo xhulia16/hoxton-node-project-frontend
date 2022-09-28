2246592155eb29cb99d39f3b2b55696f874c5440
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-28 18:41:03 +0200

    user can post

--- src/components/CreatePostForm.tsx
+++ src/components/CreatePostForm.tsx
@@ -2,11 +2,26 @@ export function CreatePostForm({currentUser}){
   if(currentUser===null)
   return <h1>loading...</h1>
     return(
-        <form className="create-post">
+        <form className="create-post" onSubmit={(event)=>{
+          event.preventDefault()
+          fetch('http://localhost:5126/posts', {
+            method: "POST", 
+            headers: {
+              "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify({
+              content: event.target.content.value,
+              image: event.target.image.value,
+              userId: currentUser.id
+            })
+          })
+
+          event.target.reset()
+        }}>
           <p>Hello {currentUser.name}!</p>
         <p>Share your thoughts with your friends...</p>
-        <textarea placeholder="What are you thinking?"></textarea>
-        <input placeholder="Add an image url"></input>
+        <textarea name="content" placeholder="What are you thinking?"></textarea>
+        <input name="image" placeholder="Add an image url"></input>
         <button>Add a post</button>
       </form>
   
