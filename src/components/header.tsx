fcfa7bbbf51f002c14fb91dc81ded7cbeede209f
Fevronia Gjini <fevronigjini1@gmail.com>
2022-09-29 15:55:38 +0200

    search bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,50 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { SearchBar } from "../pages/SearchBar";
+import { User } from "../types";
 
-export function Header({currentUser, signOutUser}){
-    return(
-        <header>
-          <Link to="/home">
+type Props = {
+  currentUser: User;
+};
+export function Header({ currentUser, signOutUser }: Props) {
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5126/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <header>
+      <Link to="/home">
         <h2 className="title">HOXTALIZING</h2>
-        </Link>
-        <ul>
-          <li>BOOKMARKS</li>
-          <li>PROFILE</li> 
-          {currentUser? (<li><button onClick={signOutUser}>LogOut</button></li>): 
-          (<>
-          <Link to="/signIn">
-          <li>SignIN</li>
-          </Link>
-          <Link to="/signUp">
-          <li>SignUp</li>
-          </Link>
-          </>)
-          }
-          
-          <li className="contact-us">CONTACT US</li>
-        </ul>
-      </header>
-    )
+      </Link>
+      <ul>
+        <li>BOOKMARKS</li>
+        <li>PROFILE</li>
+        {currentUser ? (
+          <li>
+            <button onClick={signOutUser}>LogOut</button>
+          </li>
+        ) : (
+          <>
+            <Link to="/signIn">
+              <li>SignIN</li>
+            </Link>
+            <Link to="/signUp">
+              <li>SignUp</li>
+            </Link>
+          </>
+        )}
+
+        <li className="contact-us">CONTACT US</li>
+        <SearchBar setSearch={setSearch} />
+      </ul>
+    </header>
+  );
 }
---
+++ src/pages/SearchBar.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export function SearchBar({ setSearch }: Props) {
+  return (
+    <input
+      className="search"
+      placeholder="Search For a User..."
+      onChange={(event) => {
+        setSearch(event.target.value);
+      }}
+    />
+  );
+}
